c394d3e515dfba216a7c4ed15685beac432972f8
deanv <Annhi1975@gmail.com>
2024-07-12 09:22:49 +0700

    -Feature rework -Redo both ball and player movement

--- Scene/Game Object/ball.gd
+++ Scene/Game Object/ball.gd
@@ -1,10 +1,20 @@
 extends RigidBody2D
 
-@export var speed : float = 50
+@export var max_time_increase_speed : int 
+var allowed : bool
+var number_of_time = 0 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	start()
+	linear_velocity = Vector2(100, -100)
 
-func start():
-	linear_velocity = Vector2(1, -1) * speed
+func increase_speed(speed : int) -> void:
+	linear_velocity *= 1.3
+
+func _on_body_entered(body):
+	if allowed and number_of_time < max_time_increase_speed:
+		number_of_time += 1
+		increase_speed(2)
+		allowed = false
+
+func _on_increase_speed_cooldown_timeout():
+	allowed = true
