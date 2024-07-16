f5aa58157128f4e362fad4bff6bcdb8c6f28e3d5
deanv <Annhi1975@gmail.com>
2024-07-16 07:10:34 +0700

    complete dynamic to static typing transition

--- enemy.gd
+++ enemy.gd
@@ -1,26 +1,26 @@
 extends RigidBody2D
 
-var hp = 5
+var hp : int = 5
 
-func _ready():
+func _ready() -> void:
 	$DeathAnimation.hide()
 	$DeathAnimation.stop()
-	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
+	var mob_types : Array = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
 
 
-func _process(delta):
+func _process(delta : float) -> void:
 	if Input.is_action_pressed("space"):
 		mob_death()
 
 
-func on_hit():
+func on_hit() -> void:
 	hp -= 1
 	if hp <= 0:
 		mob_death()
 
 
-func mob_death():
+func mob_death() -> void:
 	$AnimatedSprite2D.hide()
 	$DeathAnimation.show()
 	$DeathAnimation.play()
